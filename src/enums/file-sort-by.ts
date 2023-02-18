cfbccdeb8478f530179c736155605bd69306f655
Creend <creend42@gmail.com>
2023-02-18 16:21:20 +0100

    Create type for fileSorting allowed properties and update sort guard

---
+++ src/enums/file-sort-by.ts
@@ -0,0 +1,2 @@
+export const fileSortProperties = ['updatedAt', 'createdAt', 'fileSize'];
+export type FileSortBy = typeof fileSortProperties[number];
--- src/guards/sort.guard.ts
+++ src/guards/sort.guard.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { SortType } from 'src/enums/sort.type';
+import { fileSortProperties } from '../enums/file-sort-by';
 
 @Injectable()
 export class SortGuard implements CanActivate {
@@ -14,10 +15,18 @@ export class SortGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
     const sortType = request?.query?.sort ?? null;
+    const sortBy = request?.query?.sort_by ?? null;
     if (sortType && sortType !== SortType.asc && sortType !== SortType.desc) {
       throw new BadRequestException('Błędnie podany argument sortowania');
-    } else {
-      return true;
     }
+    if (sortBy && sortBy !== 'updatedAt' && sortBy !== '') {
+      const isNotValidSortBy = !!fileSortProperties.find(
+        (prop) => prop === sortBy,
+      );
+      if (isNotValidSortBy) {
+        throw new BadRequestException('Błędnie podane pole do sortowania');
+      }
+    }
+    return true;
   }
 }
